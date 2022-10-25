19ccf2289d5d5101fe8ab38caad4db0f8c362132
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-10-25 23:22:07 +0300

    25.10.2022

--- src/test/java/Get01.java
+++ src/test/java/Get01.java
@@ -52,11 +52,33 @@ public class Get01 {
         Response response= given().when().get(url);
         response.prettyPrint();
 
-        // iv)Do Asserion (dogrulama yapmak)
+        // iv) Do Assertion (dogrulama yapmak)
+
         response.then().assertThat().statusCode(200).contentType("application/json").statusLine("HTTP/1.1 200 OK");
+
+        // Status Code konsola yazdiralim
+        System.out.println("Status Code : "+response.getStatusCode());
+
+        // Content Type konsola yazdiralim
+        System.out.println("Content Type : "+ response.getContentType());
+
+        // Status Line konsola yaziralim
+        System.out.println("Status Line : "+response.getStatusLine());
+
+        // Header konsola yazdiralim
+        System.out.println("Header : "+response.getHeader("Server"));
+
+        // Headers konsola yazdiralim
+        System.out.println("Headers : "+response.getHeaders());
+
+        // Time konsola yazdiralim
+
+        System.out.println("Time : "+response.getTime());
+
+    }
 
 
 
     }
 
-}
+
---
+++ src/test/java/Get02.java
@@ -0,0 +1,48 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class Get02 {
+
+   /* Given
+    https://restful-booker.herokuapp.com/booking/1
+    When
+    User send a GET Request to the url
+    Then
+    HTTP Status code should be 404
+    And
+    Status Line should be HTTP/1.1 404 Not Found
+    And
+    Response body contains "Not Found"
+    And
+    Response body does not contain "TechProEd"
+    And
+    Server is "Cowboy"
+  */
+
+    @Test
+    public void get01() {
+        //  i)Set the URL,
+        String url="https://restful-booker.herokuapp.com/booking/1";
+
+        //  ii)Set the expected Data (beklenen datanın olusturulmasi, bunlari Post, Put, Patch ile yapacagiz)
+        // iii)Type code to send request (Talep göndermek icin kod yazimi)
+        Response response=given().when().get(url);
+        response.prettyPrint();
+
+        // iv) Do Assertion (dogrulama yapmak)
+        response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+
+        // Bady Not Found iceriyormu testi yapılıyor.
+        assertTrue(response.asString().contains("Not Found"));
+
+        // Bady'nin  TechProEd icermiyor mu testi yapılıyor.
+        assertFalse(response.asString().contains("TechProEd"));;
+
+        //Server'in Cowboy olup olmadigini test ediyoryz.
+        assertEquals("Cowboy",response.getHeader("Server"));
+
+    }
+}
